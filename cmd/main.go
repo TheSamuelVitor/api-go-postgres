1f4371fed68dee711a11a340f551467005770304
TheSamuelVitor <71853407+TheSamuelVitor@users.noreply.github.com>
2022-07-06 16:01:30 -0300

    feat: maintenance port

--- cmd/main.go
+++ cmd/main.go
@@ -9,13 +9,14 @@ import (
 	"github.com/TheSamuelVitor/api-go-postgres/pkg/handlers/teams"
 	"github.com/gin-gonic/gin"
 	"github.com/spf13/viper"
+	"os"
 )
 
 func main() {
 	viper.SetConfigFile("./pkg/common/envs/.env")
 	viper.ReadInConfig()
 
-	port := viper.Get("PORT").(string)
+	port := os.Getenv("PORT")
 	dbUrl := viper.Get("DB_URL").(string)
 
 	r := gin.Default()
