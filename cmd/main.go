45228b916dbe6473e70c5c80651171b1b84412a3
TheSamuelVitor <71853407+TheSamuelVitor@users.noreply.github.com>
2022-07-06 16:08:36 -0300

    up: port correction

--- cmd/main.go
+++ cmd/main.go
@@ -9,14 +9,13 @@ import (
 	"github.com/TheSamuelVitor/api-go-postgres/pkg/handlers/teams"
 	"github.com/gin-gonic/gin"
 	"github.com/spf13/viper"
-	"os"
 )
 
 func main() {
 	viper.SetConfigFile("./pkg/common/envs/.env")
 	viper.ReadInConfig()
 
-	port := os.Getenv("PORT")
+	port := viper.Get("PORT").(string)
 	dbUrl := viper.Get("DB_URL").(string)
 
 	r := gin.Default()
