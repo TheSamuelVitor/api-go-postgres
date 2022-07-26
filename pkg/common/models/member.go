56d13cce9849e66511f81b240effd8593deb0815
TheSamuelVitor <71853407+TheSamuelVitor@users.noreply.github.com>
2022-07-26 16:07:26 -0300

    update models

--- pkg/common/models/member.go
+++ pkg/common/models/member.go
@@ -1,7 +1,7 @@
 package models
 
 type Membro struct {
-	Id_membro   uint    `gorm:primary_key`  `json:"id_membro"`
+	Id_membro   uint    `gorm:"primary_key" json:"id_membro"`
 	Name_member string `json:"name_member"`
 	Function    string `json:"function"`
 	Id_equipe   uint    `json:"id_equipe"`
--- pkg/common/models/task.go
+++ pkg/common/models/task.go
@@ -1,7 +1,7 @@
 package models
 
 type Tarefa struct {
-	Id_tarefa   uint    `gorm:"primary_key"` `json:"id_tarefa"`
+	Id_tarefa   uint    `gorm:"primary_key" json:"id_tarefa"`
 	Name_task   string `json:"name_task"`
 	Description string `json:"description"`
 	Id_projeto  int    `json:"id_project"`
