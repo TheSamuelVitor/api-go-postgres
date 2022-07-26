a0eb9c00ea6e82b4c95257a376c4479239267df6
TheSamuelVitor <71853407+TheSamuelVitor@users.noreply.github.com>
2022-07-26 16:08:46 -0300

    update models

--- pkg/common/models/project.go
+++ pkg/common/models/project.go
@@ -1,7 +1,7 @@
 package models
 
 type Projeto struct {
-	Id_projeto   uint   `gorm:"primary_key" json:"id_projeto"`
+	Id_projeto   int   `gorm:"primary_key" json:"id_projeto"`
 	Name_project string `json:"name_project"`
 	Description  string `json:"description"`
 	Id_equipe    int    `json:"id_team"`
