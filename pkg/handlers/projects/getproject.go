e4b68d3d12293bcd0e78839b6d1e2d62f1c09115
TheSamuelVitor <71853407+TheSamuelVitor@users.noreply.github.com>
2022-07-28 10:46:00 -0300

    Revert "add creation date and edit the gets"
    This reverts commit 4d8b547fbe32724b68f8c6591c22025b292b2884.

--- pkg/handlers/projects/getproject.go
+++ pkg/handlers/projects/getproject.go
@@ -9,7 +9,7 @@ import (
 
 func (h handler) GetProjects(c *gin.Context) {
 	var projects []models.Projeto
-	sql := "select projetos.id_projeto, projetos.name_project, projetos.description, equipes.id_equipe, equipes.name_team  from projetos join equipes on equipes.id_equipe = projetos.id_equipe;"
+	sql := "select * from projetos"
 
 	if result := h.DB.Raw(sql).Scan(&projects); result.Error != nil {
 		c.AbortWithError(http.StatusNotFound, result.Error)
