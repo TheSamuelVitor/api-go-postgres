2dcdc94b56ba1a3b1f21e2341f4002d93f48a123
TheSamuelVitor <71853407+TheSamuelVitor@users.noreply.github.com>
2022-08-12 16:34:17 -0300

    update member functions and structure

--- pkg/common/models/project.go
+++ pkg/common/models/project.go
@@ -5,5 +5,12 @@ type Projeto struct {
 	Nome_projeto string `json:"nome_projeto"`
 	Descricao    string `json:"descricao"`
 	Id_equipe    int    `json:"id_equipe"`
+}
+
+type ProjetoCompleto struct {
+	Id_projeto   int    `gorm:"primary_key" json:"id_projeto"`
+	Nome_projeto string `json:"nome_projeto"`
+	Descricao    string `json:"descricao"`
+	Id_equipe    int    `json:"id_equipe"`
 	Nome_equipe  string `json:"nome_equipe"`
 }
--- pkg/handlers/projects/getprojectbyid.go
+++ pkg/handlers/projects/getprojectbyid.go
@@ -10,14 +10,11 @@ import (
 func (h handler) GetProjectbyId(c *gin.Context) {
 	id := c.Param("id")
 
-	var project models.Projeto
-	sql := "select * from projetos where id_projeto = ?"
+	var project models.ProjetoCompleto
+	sql := "select projetos.id_projeto, projetos.nome_projeto, projetos.descricao, projetos.id_equipe, equipes.nome_equipe from projetos join equipes on projetos.id_equipe = equipes.id_equipe where id_projeto = ?"
 
 	if result := h.DB.Raw(sql, id).Scan(&project); result.Error != nil {
 		c.AbortWithError(http.StatusNotFound, result.Error)
-		c.JSON(http.StatusNotFound, gin.H{
-			"message": "project not found",
-		})
 		return
 	}
 
