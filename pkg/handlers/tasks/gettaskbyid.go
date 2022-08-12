e3aba5e618638ef7a665eacd7bbe837e7c2289c7
TheSamuelVitor <71853407+TheSamuelVitor@users.noreply.github.com>
2022-08-12 19:20:17 -0300

    Revert "update task function and model"
    This reverts commit 5132d68f017de8b8b4352534cb516dd7be68c75e.

--- pkg/handlers/tasks/gettaskbyid.go
+++ pkg/handlers/tasks/gettaskbyid.go
@@ -10,11 +10,14 @@ import (
 func (h handler) GetTaskbyId(c *gin.Context) {
 	id := c.Param("id")
 
-	var task models.TarefaCompleta
-	sql := "select tarefas.id_tarefa, tarefas.nome_tarefa, tarefas.descricao, membros.id_membro,  membros.nome_membro,projetos.id_projeto, projetos.nome_projeto from tarefas join membros on membros.id_membro = tarefas.id_membro join projetos on projetos.id_projeto = tarefas.id_projeto where id_tarefa = ?"
+	var task models.Tarefa
+	sql := "select * from tarefas where id_tarefa = ?"
 
 	if result := h.DB.Raw(sql, id).Scan(&task); result.Error != nil {
 		c.AbortWithError(http.StatusNotFound, result.Error)
+		c.IndentedJSON(http.StatusOK, gin.H{
+			"message": "task not found",
+		})
 		return
 	}
 
