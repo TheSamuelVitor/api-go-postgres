e09b406bf8221c9537157086f75b43d6ee6a8b88
TheSamuelVitor <71853407+TheSamuelVitor@users.noreply.github.com>
2022-08-12 19:39:24 -0300

    up

--- pkg/handlers/projects/getproject.go
+++ pkg/handlers/projects/getproject.go
@@ -8,8 +8,13 @@ import (
 )
 
 func (h handler) GetProjects(c *gin.Context) {
+<<<<<<< HEAD
 	var projects []models.ProjetoCompleto
 	sql := "select projetos.id_projeto, projetos.nome_projeto, projetos.descricao, projetos.id_equipe, equipes.nome_equipe from projetos join equipes on projetos.id_equipe = equipes.id_equipe order by id_projeto;"
+=======
+	var projects []models.Projeto
+	sql := "select * from projetos"
+>>>>>>> parent of be0cd07 (update gets and models to show the respective caleed names)
 
 	if result := h.DB.Raw(sql).Scan(&projects); result.Error != nil {
 		c.AbortWithError(http.StatusNotFound, result.Error)
