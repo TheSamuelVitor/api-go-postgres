e05830dbc58ed8a7d80c4476f0bfa070331a1fc1
TheSamuelVitor <71853407+TheSamuelVitor@users.noreply.github.com>
2022-08-13 16:35:35 -0300

    add TarefacomProjetoeMembro struct

--- pkg/common/models/task.go
+++ pkg/common/models/task.go
@@ -5,7 +5,16 @@ type Tarefa struct {
 	Nome_tarefa string `json:"nome_tarefa"`
 	Descricao   string `json:"descricao"`
 	Id_projeto  int    `json:"id_projeto"`
+	Id_membro   int    `json:"id_membro"`
+}
+
+type TarefacomProjetoeMembro struct {
+	Id_tarefa   int    `gorm:"primary_key" json:"id_tarefa"`
+	Nome_tarefa string `json:"nome_tarefa"`
+	Descricao   string `json:"descricao"`
+	Id_projeto  int    `json:"id_projeto"`
 	Nome_projeto string `json:"nome_projeto"`
 	Id_membro   int    `json:"id_membro"`
 	Nome_membro string `json:"nome_membro"`
 }
+	
