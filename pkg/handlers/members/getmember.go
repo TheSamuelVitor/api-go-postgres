8edd1fb16aa548b479bd894f325794146c4c4436
TheSamuelVitor <71853407+TheSamuelVitor@users.noreply.github.com>
2022-08-13 08:40:19 -0300

    Revert "update gets and models to show the respective caleed names"
    This reverts commit be0cd07509e0315cc8661606edbec33cb9432a09.

--- pkg/handlers/members/getmember.go
+++ pkg/handlers/members/getmember.go
@@ -10,13 +10,9 @@ import (
 func (h handler) GetMembers(c *gin.Context) {
 	var membro []models.MembroCompleto
 
-<<<<<<< HEAD
-	sql := "select m.id_membro, m.nome_membro, m.funcao, m.id_equipe, e.nome_equipe from membros m join equipes e on e.id_equipe = m.id_equipe order by id_membro;" 
+	sql := "select membros.id_membro, membros.nome_membro, membros.funcao, membros.id_equipe, equipes.nome_equipe from membros join equipes on equipes.id_equipe = membros.id_equipe order by id_membro;"
 
 	if result := h.DB.Raw(sql).Scan(&membro); result.Error != nil {
-=======
-	if result := h.DB.Find(&membro); result.Error != nil {
->>>>>>> parent of be0cd07 (update gets and models to show the respective caleed names)
 		c.AbortWithError(http.StatusNotFound, result.Error)
 		return
 	}
